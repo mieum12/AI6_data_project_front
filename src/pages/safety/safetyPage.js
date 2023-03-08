44f07eca490e9935a61225a4b953f167d4c6d900
[AI6/1반] 최지원 <qpdlqltb1215@gmail.com>
2023-03-08 18:29:21 +0900

    feat: 시설소개,안전도 페이지 틀

---
+++ src/pages/safety/safetyPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+function safetyPage() {
+  return (
+    <div>
+      <SaftyGraphs>
+        
+      </SaftyGraphs>
+    </div>
+  );
+}
+
+const SaftyGraphs = styled.div`
+
+`
+
+export default safetyPage;
---
+++ src/pages/shelter/shelterListPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+function shelterListPage() {
+  return (
+    <div>
+      <ShelterList>
+        
+      </ShelterList>
+    </div>
+  );
+}
+
+const ShelterList = styled.div`
+
+`
+
+export default shelterListPage;
