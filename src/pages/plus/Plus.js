2ace5005313670dcf28177d0750cf9cc2540ae1e
[AI6/1반] 최지원 <qpdlqltb1215@gmail.com>
2023-03-09 21:55:38 +0900

    fix: plus page 수정

--- src/pages/plus/Plus.js
+++ src/pages/plus/Plus.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Header from "../../components/Header";
 
@@ -8,15 +9,18 @@ function Plus() {
       <Header />
       <PlusList>
         <div className="Box">
-          국민 재난 안전 포털 <br/>
-          링크: https://www.safekorea.go.kr/ <br/>
-          재난 발생 정보 조회, 비상 연락망, 재난 안전 상황 정보 등 확인 가능
+          <img src='/국민재난안전포털.jpeg' alt="플러스페이지사진"/> 
+          <h3><Link to="https://www.safekorea.go.kr/" target="_blank">국민 재난 안전 포털</Link></h3>
+          <div>재난 발생 정보 조회 및 재난 안전 상황 정보 등의 재난 현황을 확인할 수 있으며 또 다른 안전시설 등의 확인이 가능합니다.</div>
         </div>
+
         <div className="Box">
-          행정 안전부 <br/>
-          링크: https://www.mois.go.kr/ <br/>
-          (더 찾아보고 작성하기) 이재민 관련 지원 사업 신속 보도자료 등 확인
+        <img src='/행정안전부.jpeg' alt="플러스페이지사진"/>
+          <h3><Link to="https://www.mois.go.kr/" target="_blank">행정 안전부</Link></h3>
+          <div>국가 안전 시스템 정책 소개 및 온라인 민원 신청 안내를 받을 수 있습니다.</div><br/>
+          <div>이재민 관련 지원 사업 신속 보도자료 등 확인이 가능합니다.</div>
         </div>
+
         <div className="Box">3</div>
       </PlusList>
     </>
@@ -31,14 +35,21 @@ background-color: #f8f5f1;
 .Box{
   display: flex;
   align-self: center;
+  flex-direction: column;
   width: 200px;
-  height: 300px;
+  height: 400px;
+  
   color: #f8f5f1;
   background: #425f57;
   margin: 20px;
   padding: 20px;
   border-radius: 20px;
   
+}
+img {
+  width: 200px;
+  height: 200px;
+  border-radius: 20px;
 }
 `;
 export default Plus;
