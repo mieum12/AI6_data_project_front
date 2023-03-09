e5772067a51a9c0c233e02b3b7b91da85ac6ef3c
[AI6/1반] 최지원 <qpdlqltb1215@gmail.com>
2023-03-09 17:48:34 +0900

    feat: 기본 kakao map 삽입

--- src/components/ShelterMap.js
+++ src/components/ShelterMap.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 
+const{kakao}=window;
+
 function ShelterMap() {
+  useEffect(()=>{
+    const mapContainer = document.getElementById('map'); // 지도를 표시할 div 
+    const mapOption = { 
+      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+      level: 3 // 지도의 확대 레벨
+    };
+    // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+    const map = new kakao.maps.Map(mapContainer, mapOption); 
+  },[])
+
   return (
-    <Map> 지도 가져올 곳
+    <Map id="map" style={{width:'340',height:'390'}}> 지도 가져올 곳
     </Map>
   );
 }
