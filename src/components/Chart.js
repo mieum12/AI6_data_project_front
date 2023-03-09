427fb2915b22545be616c2138b62902b87ddd9ec
[AI6/2반] 김도하 <alice.dohaa@gmail.com>
2023-03-09 23:29:52 +0900

    fix:safety page 수정

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -3,36 +3,43 @@ import styled from "styled-components";
 export default function Chart() {
   return (
     <>
+      <Background></Background>
       <ChartSection>
         <div className="chartTitle">재난 발생 현황</div>
-        <div className="chartBack"></div>
+        <div className="chartBack">재난 누적 그래프</div>
       </ChartSection>
       <ChartSection>
         <div className="chartTitle">수용 시설 현황</div>
-        <div className="chartBack"></div>
+        <div className="chartBack">통합 자치구 수용시설 그래프</div>
       </ChartSection>
       <SafetySection>
         <div className="back"></div>
         <ul className="cardWrapper">
-          <li className="card">a</li>
-          <li className="card">b</li>
-          <li className="card">c</li>
+          <li className="card">a동</li>
+          <li className="card">b동</li>
+          <li className="card">c동</li>
         </ul>
       </SafetySection>
     </>
   );
 }
 
+const Background = styled.div`
+  background-color: #f8f5f1;
+  width: 100%;
+  height: 1800px;
+
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  z-index: -2;
+`;
+
 const ChartSection = styled.div`
-  margin: 40px 0 80px 0;
+  margin: 20px 0 80px 0;
 
   .chartTitle {
-    /* position: absolute;
-    top: 170px;
-    left: 45%; */
-
     margin: auto;
-    margin-top: 40px;
 
     text-align: center;
     font-size: 32px;
@@ -47,12 +54,13 @@ const ChartSection = styled.div`
   }
 
   .chartBack {
-    /* position: absolute; */
-    /* top: 270px; */
+    text-align: center;
+    color: #f8f5f1;
 
     margin: auto;
     margin-top: 50px;
     display: block;
+    padding-top: 30px;
 
     width: 655px;
     height: 408.5px;
@@ -66,12 +74,13 @@ const SafetySection = styled.div`
 
   .cardWrapper {
     margin: 40px;
+    margin-bottom: 0;
     text-align: center;
   }
 
   .card {
     display: inline-block;
-    margin: 50px 20px 60px 20px;
+    margin: 50px 70px 60px 70px;
 
     width: 199px;
     height: 255.5px;
@@ -88,7 +97,7 @@ const SafetySection = styled.div`
     z-index: -1;
 
     width: 100%;
-    height: 439px;
+    height: 600px;
 
     background: #425f57;
   }
