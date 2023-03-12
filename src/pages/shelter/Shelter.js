b6402a906eeb4be433fa6f33f38637bc688d7e1f
[AI6/1반] 최지원 <qpdlqltb1215@gmail.com>
2023-03-12 14:23:08 +0900

    fix: shelter 경로 대소문자 수정

--- src/pages/shelter/Shelter.js
+++ src/pages/shelter/Shelter.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Header from "../../components/Header";
 import ShelterMap from "../../components/ShelterMap";
-import ShelterList from "../../components/ShelterList";
+import ShelterList from "../../components/shelterList";
 
 function Shelter() {
   return (
