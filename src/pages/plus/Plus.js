a89efbebea907dcbbc138b598628dcedfa576b98
[AI6/1반] 최지원 <qpdlqltb1215@gmail.com>
2023-03-12 14:44:58 +0900

    plus page 수정

--- src/pages/plus/Plus.js
+++ src/pages/plus/Plus.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
 import Header from "../../components/Header";
 import PlusBody from "../../components/PlusBody";
 
