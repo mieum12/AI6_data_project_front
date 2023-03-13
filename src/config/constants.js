173d3379e20d4f81e248c21a9ad02e4827f14c0d
[AI6/2반] 김도하 <alice.dohaa@gmail.com>
2023-03-14 01:23:59 +0900

    fix: primary color 상수로 정의

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,8 +1,3 @@
-export const LANDING_PAGE = "/";
-export const SHELTER_PAGE = "/ShelterPage";
-export const PLUS_PAGE = "/PlusPage";
-export const SAFETY_PAGE = "/SafetyPage";
-
-// 초록색, 흰색
-// export const B_GREEN = "#425f57";
-// export const B_WHITE = "#f8f5f1";
+// 기본 컬러 상수로 만듦
+export const PRIMARY_COLOR_GREEN = "#425f57";
+export const PRIMARY_COLOR_WHITE = "#f8f5f1";
