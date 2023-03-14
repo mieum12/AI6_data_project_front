a6c8026d144cd786fd629498afd1701ef5fe229e
[AI6/1반] 최지원 <qpdlqltb1215@gmail.com>
2023-03-14 20:14:10 +0900

    shelter page에 세계지도 일단 넣어보기(서울지도로 바꾸기)

--- src/components/shelter/ShelterList.js
+++ src/components/shelter/ShelterList.js
@@ -1,14 +1,12 @@
 import * as S from "./ShelterList.style";
 import Form from "../Form";
-// import ShelterRoadview from "./ShelterRoadview";
+import MapChart from "./SeoulMap";
 
 export const ShelterList = () => {
   return (
     <S.ListTable>
-      {/* <div className={styles.listTable}> 수용시설 리스트 표 가져올 곳 */}
       <Form />
-      {/* <ShelterRoadview/> */}
-      {/* </div> */}
+      <MapChart/>
     </S.ListTable>
   );
 };
