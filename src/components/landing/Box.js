d901b9fa0fbe642e8d568cc8bb74acbeb3850861
[AI6/1반] 최지원 <qpdlqltb1215@gmail.com>
2023-03-15 13:59:23 +0900

    fix: plus body text

--- src/components/landing/Box.js
+++ src/components/landing/Box.js
@@ -2,7 +2,7 @@ import React from "react";
 import * as S from "./Box.style";
 import { Link } from "react-router-dom";
 
-export const Box = ({ boxImage, boxLink, LinkName, boxNumber, boxText }) => {
+export const Box = ({ boxImage, boxLink, LinkName, boxNumber, boxText1,boxText2,boxText3,boxText4}) => {
   return (
     <S.BoxSection>
       <div className="BoxList">
@@ -12,7 +12,10 @@ export const Box = ({ boxImage, boxLink, LinkName, boxNumber, boxText }) => {
             <Link to={boxLink}>{LinkName}</Link>
           </h3>
           <div>{boxNumber}</div>
-          {/* <div>{boxText.map( text => <p>{text}</p> )}</div> */}
+          <div>{boxText1}</div>
+          <div>{boxText2}</div>
+          <div>{boxText3}</div>
+          <div>{boxText4}</div>
         </div>
       </div>
     </S.BoxSection>
