688002cb024553aa2e1c3ad97071f9b3ebb1468a
[AI6/1반] 최지원 <qpdlqltb1215@gmail.com>
2023-03-17 00:38:37 +0900

    fix: box text오류 수정 실패

--- src/components/landing/Box.js
+++ src/components/landing/Box.js
@@ -2,7 +2,16 @@ import React from "react";
 import * as S from "./Box.style";
 import { Link } from "react-router-dom";
 
-export const Box = ({ boxImage, boxLink, LinkName, boxNumber, boxText1,boxText2,boxText3,boxText4}) => {
+export const Box = ({
+  boxImage,
+  boxLink,
+  LinkName,
+  boxNumber,
+  boxText1,
+  boxText2,
+  boxText3,
+  boxText4,
+}) => {
   return (
     <S.BoxSection>
       <div className="BoxList">
@@ -12,11 +21,12 @@ export const Box = ({ boxImage, boxLink, LinkName, boxNumber, boxText1,boxText2,
             <Link to={boxLink}>{LinkName}</Link>
           </h3>
           <div>{boxNumber}</div>
-          {/* <div>{boxText1.map((text) => <p>{text}</p> ) }</div> */}
+          {/* <div>
+            {boxText1.split("\n").map((text) => (
+              <pre>{text}</pre>
+            ))}
+          </div> */}
           <div>{boxText1}</div>
-          <div>{boxText2}</div>
-          <div>{boxText3}</div>
-          <div>{boxText4}</div>
         </div>
       </div>
     </S.BoxSection>
--- src/components/plus/PlusBody.js
+++ src/components/plus/PlusBody.js
@@ -17,44 +17,25 @@ export function PlusBody() {
             boxImage={jaenan}
             boxLink="https://www.safekorea.go.kr/"
             LinkName="국민재난안전포털"
-            boxText1="▫️ 재난 현황 확인"
-            boxText2=": 재난 정보 조회 및 상황 정보 등"
-            boxText3="▫️ 타 안전시설 확인"
-            boxText4=": 지진 대피소, 민방위 대피소 등"
-            // boxText1={[
-            //   "▫️ 재난 현황 확인",
-            //   ": 재난 정보 조회 및 상황 정보 등",
-            //   "▫️ 타 안전시설 확인",
-            //   ": 지진 대피소, 민방위 대피소 등",
-            // ]}
+            boxText1={
+              " ▫️재난 현황 확인\n   재난 정보 조회 및 상황 정보 등\n ▫️타 안전시설 확인\n   지진 대피소, 민방위 대피소 등"
+            }
           />
           <Box
             boxImage={haengjeong}
             boxLink="https://www.mois.go.kr/"
             LinkName="행정안전부"
-            boxText1="▫️ 국가 안전 시스템 정책 소개"
-            boxText2="▫️ 온라인 민원 신청 안내"
-            boxText3="▫️ 이재민 관련 지원 사업, 신속 보도자료 등 확인"
-            // boxText1={[
-            //   "▫️ 국가 안전 시스템 정책 소개",
-            //   "▫️ 온라인 민원 신청 안내",
-            //   "▫️ 이재민 관련 지원 사업, 신속 보도자료 등 확인",
-            // ]}
+            boxText1={
+                "▫️ 국가 안전 시스템 정책 소개\n   ▫️ 온라인 민원 신청 안내\n   ▫️ 이재민 관련 지원 사업, 신속 보도자료 등 확인"
+              }
           />
           <Box
             boxImage={bangjae}
             boxLink="https://119.seoul.go.kr/"
             LinkName="서울종합방재센터(119)"
-            boxText1="▫️ 재난 정보 서비스 소개"
-            boxText2=": 재난 발생 통계, 재난 보고 등"
-            boxText3="▫️ 사고별 대응 요령 소개"
-            boxText4="▫️ 시민 참여 활동: 자원봉사"
-            // boxText1={[
-            //   "▫️ 재난 정보 서비스 소개",
-            //   ": 재난 발생 통계, 재난 보고 등",
-            //   "▫️ 사고별 대응 요령 소개",
-            //   "▫️ 시민 참여 활동: 자원봉사",
-            // ]}
+            boxText1={
+                "▫️ 재난 정보 서비스 소개\n   재난 발생 통계, 재난 보고 등\n ▫️ 사고별 대응 요령 소개\n   ▫️ 시민 참여 활동: 자원봉사"
+              }
           />
         </div>
       </S.PlusList>
