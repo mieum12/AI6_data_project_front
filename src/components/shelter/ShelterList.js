f96d97565b46bd30c24aa72e82f905af4e1d0e65
[AI6/1반] 최지원 <qpdlqltb1215@gmail.com>
2023-03-17 01:36:32 +0900

    test: shelter list 데이터받아오기 진행중

--- src/components/shelter/ShelterList.js
+++ src/components/shelter/ShelterList.js
@@ -1,12 +1,45 @@
 import * as S from "./ShelterList.style";
 import Form from "../Form";
-import MapChart from "./SeoulMap";
+// import MapChart from "./SeoulMap";
+
+//데이터 가져오기
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { SERVER_BASE_URL } from "../../config/constants";
+
+
 
 export const ShelterList = () => {
+
+  const [shelter, setShelter] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${SERVER_BASE_URL}/shelter`);
+        setShelter(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+
+
   return (
     <S.ListTable>
       <Form />
-      <MapChart/>
+      {/* <MapChart/> */}
+
+      {/* 데이터받아오기 */}
+      {shelter.map((shelter)=> (
+        <div key={shelter.shortId}>
+          <div>{shelter.guNm}</div>
+          <div>{shelter.shelter}</div>
+          <div>{shelter.address}</div>
+        </div>
+      ))}
+      
     </S.ListTable>
   );
 };
