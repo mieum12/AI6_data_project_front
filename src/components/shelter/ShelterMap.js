86bffa9b1ce35375e9d7e339b737c867ee00d9c4
[AI6/1반] 최지원 <qpdlqltb1215@gmail.com>
2023-03-17 18:51:03 +0900

    fix:kakao map maker click이벤트 시도

--- src/components/shelter/ShelterMap.js
+++ src/components/shelter/ShelterMap.js
@@ -23,13 +23,13 @@ export const ShelterMap = () => {
     const mapContainer = document.getElementById("map"); // 지도를 표시할 div
     const mapOption = {
       center: new kakao.maps.LatLng(37.551399, 126.988259), // 지도의 중심좌표=임시로 남산타워
-      level: 5, // 지도의 확대 레벨
+      level: 9, // 지도의 확대 레벨
     };
     // 지도를 표시할 div와 지도 옵션으로  지도를 생성
     const map = new kakao.maps.Map(mapContainer, mapOption);
 
     // 임의로 마커
-    var positions = shelter.map((shelter)=> {
+    const positions = shelter.map((shelter)=> {
       return {
         title: shelter.shelterNm,
         latlng: new kakao.maps.LatLng(shelter.yCord, shelter.xCord),
@@ -39,18 +39,18 @@ export const ShelterMap = () => {
 
 
     // 마커 이미지의 이미지 주소입니다
-    var imageSrc =
+    const imageSrc =
       "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
     for (var i = 0; i < positions.length; i++) {
       // 마커 이미지의 이미지 크기 입니다
-      var imageSize = new kakao.maps.Size(24, 35);
+      const imageSize = new kakao.maps.Size(24, 35);
 
       // 마커 이미지를 생성합니다
-      var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+      const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
       // 마커를 생성합니다
-      var marker = new kakao.maps.Marker({
+      const marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
         position: positions[i].latlng, // 마커를 표시할 위치
         title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
@@ -62,12 +62,12 @@ export const ShelterMap = () => {
       marker.setMap(map);
 
       // 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
-      var iwContent = positions[i].title, 
+      const iwContent = positions[i].title, 
       // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
           iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
 
       // 인포윈도우를 생성합니다
-      var infowindow = new kakao.maps.InfoWindow({
+      const infowindow = new kakao.maps.InfoWindow({
           content : iwContent,
           removable : iwRemoveable
       });
@@ -78,7 +78,7 @@ export const ShelterMap = () => {
             infowindow.open(map, marker);  
       });
     }
-  }, []);
+  }, [shelter]);
 
   return <S.Map id="map" />;
 };
