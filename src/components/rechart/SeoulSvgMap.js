402e0d8f0763fea108207d2defd4c49d514b6925
realmarcustak <taghyochang@taghyochang-ui-MacBookAir.local>
2023-03-20 00:44:17 +0900

    fix: map 클릭시 alert 문구 후 shelterPage로 이동

--- src/components/rechart/SeoulSvgMap.js
+++ src/components/rechart/SeoulSvgMap.js
@@ -2,7 +2,8 @@ import * as S from "./SeoulSvgMap.style";
 import { useEffect } from "react";
 
 // 1. path#id를 객체 배열로 생성
-
+// 구를 한글로 변환하고 싶은데 svg파일 한글로 변환하면 먹히지가 않음..
+// 유니코드를 이용하라는 거 같기도 한데 변경해서 넣어봐도 안되서 잘 모르겠습니다ㅠ
 const GU_LIST = [
   { id: "Dobong-gu" },
   { id: "Dongdaemun-gu" },
@@ -31,19 +32,34 @@ const GU_LIST = [
 ];
 
 export const SeoulMap = () => {
+  // useEffect(() => {
+  //   for (const el of GU_LIST) {
+  //     const element = document.getElementById(el.id);
+
+  //     console.log("element", element);
+  //     element.addEventListener("click", (e) => {
+  //       e.preventDefault();
+  //       alert("완료!");
+  //       console.log("e", e);
+  //       console.log(e.target.id + "  clicked !");
+  //     });
+  //   }
+  // }, []);  코치님이 짜주신 로직.
+
   useEffect(() => {
     for (const el of GU_LIST) {
       const element = document.getElementById(el.id);
 
-      // console.log("element", element);
-      element.addEventListener("click", (e) => {
-        e.preventDefault();
-        alert("완료!");
-        console.log("e", e);
-        console.log(e.target.id + "  clicked !");
-      });
+      element.addEventListener("click", click);
+    }
+    function click(e) {
+      e.preventDefault();
+      window.alert(e.target.id + "입니다!\n관련 시설을 찾고 싶은 신가요?");
+      window.location = "../ShelterPage";
+      // window.location = <Link to={ROUTE.SHELTER_PAGE.link} /> 링크 to 설정 시
+      // 화면이 이동은 하는데 http://localhost:3000/[object%20Object] 여기로 이동됩니다.
     }
   }, []);
-
+  //클릭시 alert창에 해당 문구 뜨면서 ShelterPage로 이동.
   return <S.StyledSeoul />;
 };
--- src/components/rechart/SeoulSvgMap.style.js
+++ src/components/rechart/SeoulSvgMap.style.js
@@ -2,6 +2,7 @@ import { ReactComponent as Seoul } from "../../../src/asset/Seoul_districts.svg"
 import styled from "styled-components";
 import { PRIMARY_COLOR_GREEN } from "../../config/constants";
 
+//svg파일 가져와서 바로 styled컴포넌트 () 안에 넣음.
 export const StyledSeoul = styled(Seoul)`
   width: 700px;
   height: 700px;
