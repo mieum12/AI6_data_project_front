ebfee939cf23176fe346b3c90d634a70bc3af7e4
realmarcustak <taghyochang@taghyochang-ui-MacBookAir.local>
2023-03-21 01:32:23 +0900

    fix: navigate으로 link해결 / label부분 해결중..

--- src/components/seoulmap/SeoulSvgMap.js
+++ src/components/seoulmap/SeoulSvgMap.js
@@ -1,37 +1,40 @@
 import * as S from "./SeoulSvgMap.style";
 import { useEffect } from "react";
+import { ROUTE } from "../../routes/Route";
+import { useNavigate } from "react-router-dom";
 
 // 1. path#id를 객체 배열로 생성
 // 구를 한글로 변환하고 싶은데 svg파일 한글로 변환하면 먹히지가 않음..
 // 유니코드를 이용하라는 거 같기도 한데 변경해서 넣어봐도 안되서 잘 모르겠습니다ㅠ
 const GU_LIST = [
-  { id: "Dobong-gu" },
-  { id: "Dongdaemun-gu" },
-  { id: "Eunpyeong-gu" },
-  { id: "Gangbuk-gu" },
-  { id: "Gangdong-gu" },
-  { id: "Gangseo-gu" },
-  { id: "Geumcheon-gu" },
-  { id: "Guro-gu" },
-  { id: "Gwanak-gu" },
-  { id: "Gwangjin-gu" },
-  { id: "Gangnam-gu" },
-  { id: "Jongno-gu" },
-  { id: "Jung-gu" },
-  { id: "Jungnang-gu" },
-  { id: "Mapo-gu" },
-  { id: "Nowon-gu" },
-  { id: "Seocho-gu" },
-  { id: "Seodaemun-gu" },
-  { id: "Seongbuk-gu" },
-  { id: "Seongdong-gu" },
-  { id: "Songpa-gu" },
-  { id: "Yangcheon-gu" },
-  { id: "Yeongdeungpo-gu_1_" },
-  { id: "Yongsan-gu" },
+  { id: "Dobong-gu", label: "도봉구" },
+  // { id: "Dongdaemun-gu", label: "동대문구" },
+  // { id: "Eunpyeong-gu", label: "은평구" },
+  // { id: "Gangbuk-gu", label: "강북구" },
+  // { id: "Gangdong-gu", label: "강동구" },
+  // { id: "Gangseo-gu", label: "강서구" },
+  // { id: "Geumcheon-gu", label: "금천구" },
+  // { id: "Guro-gu", label: "구로구" },
+  // { id: "Gwanak-gu", label: "관악구" },
+  // { id: "Gwangjin-gu", label: "광진구" },
+  // { id: "Gangnam-gu", label: "강남구" },
+  // { id: "Jongno-gu", label: "종로구" },
+  // { id: "Jung-gu", label: "중구" },
+  // { id: "Jungnang-gu", label: "중랑구" },
+  // { id: "Mapo-gu", label: "마포구" },
+  // { id: "Nowon-gu", label: "노원구" },
+  // { id: "Seocho-gu", label: "서초구" },
+  // { id: "Seodaemun-gu", label: "서대문구" },
+  // { id: "Seongbuk-gu", label: "성북구" },
+  // { id: "Seongdong-gu", label: "성동구" },
+  // { id: "Songpa-gu", label: "송파구" },
+  // { id: "Yangcheon-gu", label: "양천구" },
+  // { id: "Yeongdeungpo-gu_1_", label: "영등포구" },
+  // { id: "Yongsan-gu", label: "용산구" },
 ];
 
 export const SeoulMap = () => {
+  const navigate = useNavigate();
   // useEffect(() => {
   //   for (const el of GU_LIST) {
   //     const element = document.getElementById(el.id);
@@ -49,16 +52,19 @@ export const SeoulMap = () => {
   useEffect(() => {
     for (const el of GU_LIST) {
       const element = document.getElementById(el.id);
-
       element.addEventListener("click", click);
     }
     function click(e) {
       e.preventDefault();
-      window.alert(e.target.id + "입니다!\n관련 시설을 찾고 싶은 신가요?");
-      window.location = "../ShelterPage";
+      // console.log(e.target.id);
+      window.alert(e.target.getAttribute("label") + "입니다!\n관련 시설을 찾고 싶은 신가요?");
+      navigate(ROUTE.SHELTER_PAGE.link);
       // window.location = <Link to={ROUTE.SHELTER_PAGE.link} /> 링크 to 설정 시
       // 화면이 이동은 하는데 http://localhost:3000/[object%20Object] 여기로 이동됩니다.
+      // string을 받아오는데 link to는 객체로 인식되서 object가 떴음.
+      // usenavigate를 이용해서 해결.
     }
+    // eslint-disable-next-line
   }, []);
   //클릭시 alert창에 해당 문구 뜨면서 ShelterPage로 이동.
   return <S.StyledSeoul />;
