a53e1fbb31396fb7ffde30c4f63738ad00eb6149
[AI6/1반] 최지원 <qpdlqltb1215@gmail.com>
2023-03-20 16:54:06 +0900

    plus body의 box 색 수정(실패)

--- src/components/sheltermap/ShelterMap.js
+++ src/components/sheltermap/ShelterMap.js
@@ -1,8 +1,7 @@
 import * as S from "./ShelterMap.style";
-//데이터 가져오기
 import React, { useEffect } from "react";
 import { useShelter } from "../../hooks";
-
+// import { Link } from "react-router-dom";
 const { kakao } = window;
 
 export const ShelterMap = () => {
@@ -17,7 +16,7 @@ export const ShelterMap = () => {
     // 지도를 표시할 div와 지도 옵션으로  지도를 생성
     const map = new kakao.maps.Map(mapContainer, mapOption);
 
-    // 임의로 마커
+    // 마커
     const positions = shelter.map((shelter)=> {
       return {
         title: shelter.shelterNm,
@@ -51,6 +50,7 @@ export const ShelterMap = () => {
       marker.setMap(map);
 
       // 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
+      // const iwContent = <Link to='/'>positions[i].title</Link>, 
       const iwContent = positions[i].title, 
       // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
           iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
@@ -63,7 +63,7 @@ export const ShelterMap = () => {
 
       // 마커에 클릭이벤트를 등록합니다
       kakao.maps.event.addListener(marker, 'click', function() {
-            // 마커 위에 인포윈도우를 표시합니다 : 동작은 됨
+            // 마커 위에 인포윈도우를 표시합니다
             infowindow.open(map, marker);  
       });
     }
