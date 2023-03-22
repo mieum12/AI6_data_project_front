a4be9b30396204bcfccf9f8a834bd05e25ac7649
[AI6/2반] 김도하 <alice.dohaa@gmail.com>
2023-03-22 23:16:41 +0900

    delete: shelterDistrict 관련 파일 삭제

--- src/api/fetchApi.js
+++ src/api/fetchApi.js
@@ -29,14 +29,3 @@ export async function fetchShelter(guNm) {
     console.log("error: ", err);
   }
 }
-
-// TODO: shelter/강남구 이렇게 가져오는 api
-// 이렇게하는게 맞는지
-export async function fetchShelterDistrict({ guNm }) {
-  try {
-    const { data } = await api.get(`/shelter?guNm=${guNm}`);
-    return data;
-  } catch (err) {
-    console.log("error", err);
-  }
-}
