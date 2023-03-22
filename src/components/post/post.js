5accc42871be6e6a044348fdbf4445e039bbc82c
[AI6/2반] 김도하 <alice.dohaa@gmail.com>
2023-03-23 02:41:54 +0900

    rename: post.js

---
+++ src/components/post/post.js
@@ -0,0 +1,33 @@
+import { usePost } from "../../hooks";
+import * as S from "./Post.style";
+
+export const Post = () => {
+  const { post } = usePost();
+  console.log(post);
+
+  return (
+    <S.PostTable>
+      <table>
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>제목</th>
+            <th>작성시간</th>
+            <th>수정시간</th>
+          </tr>
+        </thead>
+        <tbody>
+          {post.map((post) => (
+            <tr key={post.postId}>
+              <td>{post.postId}</td>
+              <td>{post.title}</td>
+              {/* TODO: 날짜 형식 바꿔서 출력 */}
+              <td>{post.createdAt}</td>
+              <td>{post.updatedAt}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </S.PostTable>
+  );
+};
