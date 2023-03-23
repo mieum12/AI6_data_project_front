6a9e8db6d89a50b1236f1f2b461e3a6ab0d1acfd
[AI6/2반] 김도하 <alice.dohaa@gmail.com>
2023-03-24 05:24:11 +0900

    fix: 수정버튼 클릭에에 따른 모달창 구현(진행중)

--- src/components/post/PostModal.js
+++ src/components/post/PostModal.js
@@ -1,93 +1,73 @@
-// import { useState } from "react";
-import styled from "styled-components";
-import { PRIMARY_COLOR, COLOR_WHITE } from "../../styles/color";
+import { useState } from "react";
+import * as S from "./PostModal.style";
 
 export const PostModal = ({ setModalOpen, name }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [userNm, setUserNm] = useState("");
+  const [password, setPassword] = useState("");
+  const [updateState, setUpdateState] = useState(false);
+
   const onClose = () => {
     setModalOpen((prev) => !prev);
   };
-  console.log(name);
+
+  const onChangePost = (e) => {
+    e.preventDefault();
+    setUpdateState(true);
+  };
+
+  // 수정버튼 누르면 실행
+  // TODO: 수정하기 버튼 누르면 모달 창 사라짐 **수정 필요**
+  if (updateState) {
+    <S.ModalSection>
+      <input
+        type="text"
+        value={name.title}
+        placeholder="제목을 입력해주세요"
+        onChange={(e) => setTitle(...title, e.target.value)}
+      />
+      <textarea
+        value={name.content}
+        placeholder="내용을 입력해주세요"
+        onChange={(e) => setContent(...content, e.target.value)}
+      />
+      <input
+        type="text"
+        value={name.userNm}
+        placeholder="사용자명을 입력해주세요"
+        onChange={(e) => setUserNm(...userNm, e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="비밀번호를 입력해주세요"
+        value={name.password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+    </S.ModalSection>;
+    return;
+  }
+
   return (
-    <ModalSection>
+    <S.ModalSection>
       <button className="close" onClick={onClose}>
         <h2>X</h2>
       </button>
-      <ModalTitle>{name.shelterNm}</ModalTitle>
+      <S.ModalTitle>{name.shelterNm}</S.ModalTitle>
 
-      <ModalBody>
+      <S.ModalBody>
         <div>제목 : {name.title} </div>
         <div>사용자명 : {name.userNm}</div>
         <div>내용 :{name.content} </div>
+        {/* {updatePost} */}
         <div className="buttonWrapper">
-          <button className="Pbutton">수정하기</button>
+          <button onClick={onChangePost} className="Pbutton">
+            수정하기
+          </button>
+          {/* TODO: 삭제하기 누르면 비밀번호 확인 후 삭제 */}
           <button className="Pbutton">삭제하기</button>
         </div>
-      </ModalBody>
-    </ModalSection>
+      </S.ModalBody>
+    </S.ModalSection>
   );
 };
-
-const ModalSection = styled.div`
-  // 모달 창 크기
-  width: 700px;
-  height: 500px;
-  z-index: 999; // 모달 창 최상단으로 보이게 하는 코드
-
-  // 모달 창 중앙 배치
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-
-  // 모달 창 디자인
-  border-radius: 5px;
-  background-color: ${PRIMARY_COLOR};
-  color: ${COLOR_WHITE};
-  padding: 20px;
-
-  .close {
-    // 닫기 버튼 우상단 배치
-    position: absolute;
-    top: 1.5%;
-    right: 3%;
-
-    // 닫기 버튼 디자인
-    border-color: transparent;
-    background-color: transparent;
-    color: ${COLOR_WHITE};
-  }
-
-  .buttonWrapper {
-    position: absolute;
-    right: 20px;
-    bottom: 20px;
-  }
-
-  .Pbutton {
-    width: 80px;
-    padding: 7px 5px;
-    margin: 5px 10px 5px 5px;
-    font-weight: bold;
-    background-color: ${COLOR_WHITE};
-    color: ${PRIMARY_COLOR};
-    border-radius: 20px;
-    border: none;
-  }
-
-  .Pbutton:hover {
-    color: orange;
-  }
-`;
-
-const ModalTitle = styled.div`
-  text-align: center;
-  justify-content: top;
-  font-size: 30px;
-`;
-
-const ModalBody = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  font-size: 20px;
-`;
---
+++ src/components/post/PostModal.style.js
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+import { PRIMARY_COLOR, COLOR_WHITE } from "../../styles/color";
+
+export const ModalSection = styled.div`
+  // 모달 창 크기
+  width: 700px;
+  height: 500px;
+  z-index: 999; // 모달 창 최상단으로 보이게 하는 코드
+
+  // 모달 창 중앙 배치
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  // 모달 창 디자인
+  border-radius: 5px;
+  background-color: ${PRIMARY_COLOR};
+  color: ${COLOR_WHITE};
+  padding: 20px;
+
+  .close {
+    // 닫기 버튼 우상단 배치
+    position: absolute;
+    top: 1.5%;
+    right: 3%;
+
+    // 닫기 버튼 디자인
+    border-color: transparent;
+    background-color: transparent;
+    color: ${COLOR_WHITE};
+  }
+
+  .buttonWrapper {
+    position: absolute;
+    right: 20px;
+    bottom: 20px;
+  }
+
+  .Pbutton {
+    width: 80px;
+    padding: 7px 5px;
+    margin: 5px 10px 5px 5px;
+    font-weight: bold;
+    background-color: ${COLOR_WHITE};
+    color: ${PRIMARY_COLOR};
+    border-radius: 20px;
+    border: none;
+  }
+
+  .Pbutton:hover {
+    color: orange;
+  }
+`;
+
+export const ModalTitle = styled.div`
+  text-align: center;
+  justify-content: top;
+  font-size: 30px;
+`;
+
+export const ModalBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-size: 20px;
+`;
